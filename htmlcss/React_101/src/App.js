5f9181dc08c2f2c7561712daef4e7e04523763d5
Emelloyd <batangdumagoc@gmail.com>
2023-04-25 01:46:49 +0800

    ReactJS Activity

---
+++ htmlcss/React_101/src/App.js
@@ -0,0 +1,13 @@
+import Invoice from './Invoice';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Invoice />
+    </div>
+  );
+}
+
+export default App;
